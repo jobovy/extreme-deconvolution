55cd191790a923545726fb29cba99cea3a1a3b8a
jo.bovy <jo.bovy@52564da4-346f-11de-a9fe-ad1e22fc3a8a>
2010-02-19 05:54:01 +0000

    add description to setup file
    git-svn-id: https://extreme-deconvolution.googlecode.com/svn/trunk@33 52564da4-346f-11de-a9fe-ad1e22fc3a8a

--- setup.py
+++ setup.py
@@ -5,11 +5,17 @@ import re
 srcDir= 'src'
 srcFiles= [os.path.join(srcDir,file) for file in os.listdir('src') if re.split(r'\.',file)[-1] == 'c']
 
+
+longDescription= "We present a general algorithm to infer a d-dimensional distribution function given a set of heterogeneous, noisy observations or samples. This algorithm reconstructs the error-deconvolved or 'underlying' distribution function common to all samples, even when the individual samples have unique error and missing-data properties. The underlying distribution is modeled as a mixture of Gaussians, which is completely general. Model parameters are chosen to optimize a justified, scalar objective function: the logarithm of the probability of the data under the error-convolved model, where the error convolution is different for each data point. Optimization is performed by an Expectation Maximization (EM) algorithm, extended by a regularization technique and 'split-and-merge' procedure. These extensions mitigate problems with singularities and local maxima, which are often encountered when using the EM algorithm to estimate Gaussian density mixtures."
+
+
 setup(name='extreme-deconvolution',
       version='1.1',
       description='Density estimation using Gaussian mixtures in the presence of noisy, heterogeneous and incomplete data',
       author='Jo Bovy',
       author_email='jb2777@nyu.edu',
+      license='GPLv2',
+      long_description=longDescription,
       url='http://code.google.com/p/extreme-deconvolution/',
       package_dir = {'': 'py'},
       py_modules=['extreme_deconvolution'],
