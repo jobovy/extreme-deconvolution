0cd4a4914b09122b963c4f2b97957ff872a79151
jo.bovy <jo.bovy@52564da4-346f-11de-a9fe-ad1e22fc3a8a>
2010-03-13 03:46:31 +0000

    fix setup.py file
    git-svn-id: https://extreme-deconvolution.googlecode.com/svn/trunk@45 52564da4-346f-11de-a9fe-ad1e22fc3a8a

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os, os.path
 import re
 
 srcDir= 'src'
-srcFiles= [os.path.join(srcDir,file) for file in os.listdir('src') if re.split(r'\.',file)[-1] == 'c']
+srcFiles= ['src/bovy_isfin.c','src/bovy_randvec.c','src/calc_splitnmerge.c','src/logsum.c','src/minmax.c','src/normalize_row.c','src/proj_EM.c','src/proj_EM_step.c','src/proj_gauss_mixtures.c','src/splitnmergegauss.c','src/bovy_det.c']
 
 
 longDescription= "We present a general algorithm to infer a d-dimensional distribution function given a set of heterogeneous, noisy observations or samples. This algorithm reconstructs the error-deconvolved or 'underlying' distribution function common to all samples, even when the individual samples have unique error and missing-data properties. The underlying distribution is modeled as a mixture of Gaussians, which is completely general. Model parameters are chosen to optimize a justified, scalar objective function: the logarithm of the probability of the data under the error-convolved model, where the error convolution is different for each data point. Optimization is performed by an Expectation Maximization (EM) algorithm, extended by a regularization technique and 'split-and-merge' procedure. These extensions mitigate problems with singularities and local maxima, which are often encountered when using the EM algorithm to estimate Gaussian density mixtures."
