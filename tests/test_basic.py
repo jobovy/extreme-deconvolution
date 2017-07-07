8130bb8c6f441397d6c4b8d74d98f15ed8a4ac84
Jo Bovy <bovy@astro.utoronto.ca>
2017-07-07 17:47:48 -0400

    Add 1D K=1 tests with uncertainties

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -5,7 +5,7 @@ from extreme_deconvolution import extreme_deconvolution
 
 def test_single_gauss_1d_nounc():
     # Generate data from a single Gaussian, recover mean and variance
-    ndata= 1001
+    ndata= 3001
     ydata= numpy.atleast_2d(numpy.random.normal(size=ndata)).T
     ycovar= numpy.zeros_like(ydata)
     # initialize fit
@@ -20,3 +20,45 @@ def test_single_gauss_1d_nounc():
     assert numpy.fabs(initmean-0.) < tol, 'XD does not recover correct mean for single Gaussian w/o uncertainties'
     assert numpy.fabs(initcovar-1.) < tol, 'XD does not recover correct variance for single Gaussian w/o uncertainties'
     return None
+
+def test_single_gauss_1d_constunc():
+    # Generate data from a single Gaussian, recover mean and variance
+    ndata= 3001
+    ydata= numpy.atleast_2d(numpy.random.normal(size=ndata)).T
+    ycovar= numpy.ones_like(ydata)*0.25
+    ydata+= numpy.atleast_2d(numpy.random.normal(size=ndata)).T\
+        *numpy.sqrt(ycovar)
+    # initialize fit
+    K= 1
+    initamp= numpy.ones(K)
+    initmean= numpy.atleast_2d(numpy.mean(ydata)+numpy.std(ydata))
+    initcovar= numpy.atleast_3d(3.*numpy.var(ydata))
+    # Run XD
+    extreme_deconvolution(ydata,ycovar,initamp,initmean,initcovar)
+    # Test
+    tol= 3./numpy.sqrt(ndata)
+    assert numpy.fabs(initmean-0.) < tol, 'XD does not recover correct mean for single Gaussian w/o uncertainties'
+    assert numpy.fabs(initcovar-1.) < tol, 'XD does not recover correct variance for single Gaussian w/o uncertainties'
+    return None
+
+def test_single_gauss_1d_varunc():
+    # Generate data from a single Gaussian, recover mean and variance
+    ndata= 3001
+    ydata= numpy.atleast_2d(numpy.random.normal(size=ndata)).T
+    ycovar= numpy.ones_like(ydata)*\
+        numpy.atleast_2d(numpy.random.uniform(size=ndata)).T
+    ydata+= numpy.atleast_2d(numpy.random.normal(size=ndata)).T\
+        *numpy.sqrt(ycovar)
+    # initialize fit
+    K= 1
+    initamp= numpy.ones(K)
+    initmean= numpy.atleast_2d(numpy.mean(ydata)+numpy.std(ydata))
+    initcovar= numpy.atleast_3d(3.*numpy.var(ydata))
+    # Run XD
+    extreme_deconvolution(ydata,ycovar,initamp,initmean,initcovar)
+    # Test
+    tol= 5./numpy.sqrt(ndata)
+    assert numpy.fabs(initmean-0.) < tol, 'XD does not recover correct mean for single Gaussian w/o uncertainties'
+    assert numpy.fabs(initcovar-1.) < tol, 'XD does not recover correct variance for single Gaussian w/o uncertainties'
+    return None
+
